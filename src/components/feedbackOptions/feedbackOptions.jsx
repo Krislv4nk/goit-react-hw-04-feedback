14db8798affcb93b495ca5a29de5a3b20c0781fd
Krislv4nk <136389095+Krislv4nk@users.noreply.github.com>
2024-01-07 09:05:03 +0100

    fixed bugs in feedbackOptions

--- src/components/feedbackOptions/feedbackOptions.jsx
+++ src/components/feedbackOptions/feedbackOptions.jsx
@@ -1,41 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import css from './feedbackOptions.module.css';
 
 
-
-
-export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  const [selectedOption, setSelectedOption] = useState(null);
-
-  const handleClick = (option) => {
-    setSelectedOption(option);
-    onLeaveFeedback(option);
-  };
-
-  useEffect(() => {
-    if (selectedOption) {
-      console.log(`User selected: ${selectedOption}`);
-    }
-  }, [selectedOption]);
-
-  return (
-    <div className={css.buttonContainer}>
-      {options.map(option => (
-        <button className={css.button} key={option} onClick={() => handleClick(option)}>
-          {option}
-        </button>
-      ))}
-    </div>
-  );
-};
-
-
-// export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-//   <div className={css.buttonContainer}>
-//     {options.map(option => (
-//       <button className={css.button} key={option} onClick={() => onLeaveFeedback(option)}>
-//         {option}
-//       </button>
-//     ))}
-//   </div>
-// );
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <div className={css.buttonContainer}>
+    {options.map(option => (
+      <button className={css.button} key={option} onClick={() => onLeaveFeedback(option)}>
+        {option}
+      </button>
+    ))}
+  </div>
+);
